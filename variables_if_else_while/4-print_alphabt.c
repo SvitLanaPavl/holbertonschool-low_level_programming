5a783cd7018e40566dafc5182390fe35b32359d9
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-05-25 09:08:06 -0700

    Aphabet soup

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -12,16 +12,15 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-	char ch = 'a';
+	char ch;
 
-	while (ch <= 'z')
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 	if (ch == 'q' || ch == 'e')
 	{
 	continue;
 	}
 		putchar(ch);
-		ch++;
 	}
 	putchar('\n');
 	return (0);
