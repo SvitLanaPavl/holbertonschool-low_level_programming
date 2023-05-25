ebc618d23a8a1a580fbd2d27645a12426151dae9
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-05-25 13:11:53 -0700

    Putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /*
-  * main - Entry point
+  * The `main()` function is the entry point to the program.
   * description: print putchar no use of inbuilt functions
   * Return: Always 0
   */
