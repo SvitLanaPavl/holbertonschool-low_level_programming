69729badb51f0b425f8995264691877920069680
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-05-25 10:12:05 -0700

    Smile in the mirror

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print alphabet letters in reverse
+ * followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+
+int main(void)
+{
+	char letter = 'z';
+	while (letter >= 'a')
+	{
+putchar(letter);
+letter--;
+}
+putchar(*"\n");
+return (0);
+}
