7abfbc67dad2e89f82f66d7c1f4f170223c80155
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-05-25 10:14:13 -0700

    Smile in the mirror

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -15,10 +15,10 @@
 int main(void)
 {
 	char letter = 'z';
-	while (letter >= 'a')
-	{
-putchar(letter);
-letter--;
+while (letter >= 'a')
+{
+	putchar(letter);
+	letter--;
 }
 putchar(*"\n");
 return (0);
