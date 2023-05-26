74a7f1f6498ad800a6655c61d1c4f156302db174
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-05-26 07:43:27 -0700

    Alphabet

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -2,15 +2,17 @@
 #include <stdio.h>
 
 /**
- * Function print_alphabet() - Prints the alphabet to the standard output.
+ * print_alphabet - Prints the alphabet to the standard output.
  * Description: prints the lowercase alphabet to the standard output.
- * @return void
+ * Return: void
  */
-/* betty srtyle doc for function print_alphabet goes there */
+
 void print_alphabet(void)
 {
 	int letter;
+
 	letter = 97;
+
 	while (letter <= 122)
 	{
 		_putchar(letter);
