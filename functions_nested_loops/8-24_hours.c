30144ce5e00de05786c26e97e70efc2a387b5e14
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-05-26 13:55:56 -0700

    Hour and minute

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -11,9 +11,9 @@ void jack_bauer(void)
 	int hour;
 	int minute;
 
-	for (hour = 0; hour < 24; hour++)
+	for (hour = '0'; hour < '24'; hour++)
 	{
-		for (minute = 0; minute < 60; minute++)
+		for (minute = '0'; minute < '60'; minute++)
 		{
 			_putchar(hour + '0');
 			_putchar(58);
