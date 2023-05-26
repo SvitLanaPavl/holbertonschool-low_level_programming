9812d7409f37338015d8114f9332f1ae7bb06642
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-05-26 12:41:58 -0700

    Last digit

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -14,8 +14,7 @@ int print_last_digit(int digit)
 	if (last_digit < 0)
 	{
 		last_digit = last_digit * (-1);
-		_putchar(last_digit);
 	}
-	_putchar(last_digit);
+	_putchar(last_digit + '0');
 	return (last_digit);
 }
