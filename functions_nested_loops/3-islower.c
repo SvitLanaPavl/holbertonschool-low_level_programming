0f993f6419ef404fd82d53a4edb182d7ccc7d7f9
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-05-26 09:16:53 -0700

    Lower

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _islower - Checks for lowercase character.
+ * @c: the letter argument
+ * Description: prints the lowercase alphabet to the standard output.
+ * Return: 1 if c is lowercase. 0 otherwise
+ */
+int _islower(int c)
+{
+for (c = 65; c < 91; c++)
+{
+	return (0);
+}
+for (c = 97; c < 123; c++)
+{
+	return (1);
+}
+}
