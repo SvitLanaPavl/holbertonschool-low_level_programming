74a6d79ca80c1a88b876ab8f95d01558f3c720f2
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-05-26 11:37:24 -0700

    ABS

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -3,16 +3,16 @@
 
 /**
  * _abs - Computes the absolute value of an integer.
- * @int: the number argument
+ * @n: the number argument
  * Description: computes the absolute value of an integer.
  * Return: Always 0
  */
-int _abs(int n)
+int _abs(n)
 {
 	if (n < 0)
 	{
 	n = (-1) * n;
 	}
-	putchar(n);
+	_putchar(n);
 	return (0);
 }
