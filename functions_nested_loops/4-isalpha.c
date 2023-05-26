70e1541b1ff3fb7dcec9ed319f8ed9ff925645cd
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-05-26 09:57:11 -0700

    Letter or not

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isalpha - Checks for alphabetic character.
+ * @c: the letter argument
+ * Description: checks for alphabetic characters.
+ * Return: 1 if c letter. 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if ((c > 64 && c < 91) && (c > 96 && c < 123))
+	{
+		return (1);
+	}
+return (0);
+}
