0e4d3e1e7ff7f5574ae9c0ba77ff63bd51ae8559
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-05-28 15:55:59 -0700

    Table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -22,12 +22,13 @@ void times_table(void)
 		{
 		_putchar(c + '0');
 		_putchar(44);
+		_putchar(44);
 		_putchar(32);
 		}
 
 		if (c > 9)
 		{
-			_putchar( c / 10 + '0');
+			_putchar(c / 10 + '0');
 			_putchar(c % 10 + '0');
 			_putchar(44);
 			_putchar(32);
