39271b31219dfe0a1abbb01f0578246f3db6f3d1
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-05-28 14:16:46 -0700

    Hour

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -19,8 +19,26 @@ void jack_bauer(void)
 		{
 		_putchar('0' + hour1);
 		_putchar('0' + hour2);
-		}
 		_putchar(':');
+		for (minute1 = 0; minute1 < 10; minute1++)
+		{
+			for (minute2 = 0; minute2 < 10; minute2++)
+			{
+			_putchar('0' + minute1);
+			_putchar('0' + minute2);
+			_putchar('\n');
+			}
+		}
+		}
+	}
+		if (hour2 > 9)
+		{
+		hour1 = 2;
+		for (hour2 = 0; hour2 < 4; hour2++)
+			{
+			_putchar('0' + hour1);
+			_putchar('0' + hour2);
+			_putchar(':');
 
 		for (minute1 = 0; minute1 < 10; minute1++)
 		{
@@ -28,8 +46,8 @@ void jack_bauer(void)
 			{
 			_putchar('0' + minute1);
 			_putchar('0' + minute2);
+			_putchar('\n');
 			}
-		_putchar('\n');
 		}
-	}
-}
+			}
+		}
