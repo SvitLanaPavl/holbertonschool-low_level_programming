f07b5cea53a8044963e14dd4d951ddfdebd30288
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-05-28 20:03:04 -0700

    The world

---
+++ functions_nested_loops/100-times_table.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_times_table - Prints the n times table starting with 0.
+ * Description: if prints the n times table.
+ * @n: the operand
+ * Return: Always 0
+ */
+
+void print_times_table(int n)
+{
+	int a, b;
+
+	scanf("%d", &n);
+
+	for (b = 0; b <= 14; b++)
+	{
+		printf("%dx%d = %d, ", a, b, b * a);
+	}
+	printf("\n");
+	if (n > 15 || n < 0)
+	{
+		continue;
+	}
+}
+}
