a30262930e4964a3d946dfc08b36a2eb14c9d282
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-05-29 18:03:32 -0700

    Quote

---
+++ hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 1
+ */
+int main() {
+  char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+  size_t size = strlen(quote);
+  size_t count = 1;
+
+  fwrite(quote, size, count, stderr);
+return (1);
+}
