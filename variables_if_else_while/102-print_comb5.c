bfabbf1a0dc319b91f7634beacf6a9c72937c354
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-05-30 20:56:17 -0700

    2 digit combinations

--- variables_if_else_while/102-print_comb5.c
+++ variables_if_else_while/102-print_comb5.c
@@ -11,30 +11,24 @@
 
 int main(void)
 	{
-	int digit1, digit2, digit3, digit4;
+	int digit1, digit2;
 
-	for (digit1 = 0; digit1 <= 9; digit1++)
-	{
-	for (digit2 = 0; digit2 <= 8; digit2++)
-	{
-		for (digit3 = 0; digit3 <= 9; digit3++)
-		{
-			for (digit4 = digit2 + 1; digit4 <= 9; digit4++)
-			{
-				putchar(digit1 + '0');
-				putchar(digit2 + '0');
-				putchar(32);
-				putchar(digit3 + '0');
-				putchar(digit4 + '0');
+for (digit1 = 0; digit1 <= 99; digit1++)
+{
+for (digit2 = digit1 + 1; digit2 <= 99; digit2++)
+{
+	putchar(digit1 / 10 + '0');
+	putchar(digit1 % 10 + '0');
+	putchar(32);
+	putchar(digit2 / 10 + '0');
+	putchar(digit2 % 10 + '0');
 
-	if (digit1 == 9 && digit2 == 8 && digit3 == 9 && digit4 == 9)
+	if (digit1 == 98 && digit2 == 99)
 	{
 		continue;
 	}
 	putchar(44);
 	putchar(32);
-	}
-	}
 	}
 	}
 putchar(*"\n");
