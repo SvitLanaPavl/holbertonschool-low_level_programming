d8c8b3873f39b640aedeecd80decb6e1297c0372
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-05-30 20:39:03 -0700

    2

--- variables_if_else_while/102-print_comb5.c
+++ variables_if_else_while/102-print_comb5.c
@@ -15,11 +15,11 @@ int main(void)
 
 	for (digit1 = 0; digit1 <= 9; digit1++)
 	{
-	for (digit2 = 0; digit2 <= 9; digit2++)
+	for (digit2 = 0; digit2 <= 8; digit2++)
 	{
 		for (digit3 = 0; digit3 <= 9; digit3++)
 		{
-			for (digit4 = digit2 + 1; digit4 <= 8; digit4++)
+			for (digit4 = digit2 + 1; digit4 <= 9; digit4++)
 			{
 				putchar(digit1 + '0');
 				putchar(digit2 + '0');
