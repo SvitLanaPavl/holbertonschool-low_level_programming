76edd545948b558ed034c968f16618c7d23b3519
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-05-31 10:02:04 -0700

    Print more numbers

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * more_numbers - Prints 10 times the numbers
+ * from 0 to 14 to the standard output.
+ * Description: prints the numbers from 0 to 14
+ * to the standard output 10 times.
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int number;
+	int count;
+
+for (count = 1; count <= 10; count++)
+{
+	for (number = 1; number <= 14; number++)
+	{
+		_putchar(number + '0');
+		}
+	}
+}
+	_putchar('\n');
+}
+
