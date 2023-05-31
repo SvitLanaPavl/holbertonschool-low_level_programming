5ffa7c53f0662b4e9dafe26aaecae4c60c7d7eda
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-05-31 09:29:08 -0700

    Print numbers but 2 nd 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - Prints the numbers to the standard output.
+ * Description: prints the numbers from 0 to 9 to the standard output.
+ * Does not print 2 and 4.
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int number = 48;
+
+	while (number <= 57)
+	{
+		if (number == 50 && number == 52)
+		{
+			continue;
+		}
+		_putchar(number);
+		number++;
+	}
+	_putchar('\n');
+}
