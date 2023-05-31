1ebe3f1ac6829a352e260deddaba3de069625267
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-05-31 09:30:29 -0700

    Print most numbers

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 
 /**
- * print_numbers - Prints the numbers to the standard output.
+ * print_most_numbers - Prints the numbers to the standard output.
  * Description: prints the numbers from 0 to 9 to the standard output.
  * Does not print 2 and 4.
  * Return: void
  */
 
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int number = 48;
 
