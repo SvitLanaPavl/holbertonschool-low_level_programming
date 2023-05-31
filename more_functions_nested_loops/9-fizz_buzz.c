c1dc077e9f454a80b271f5f81f0db665a20a590b
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-05-31 14:25:12 -0700

    Fizz-Buzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ * Description: prints the numbers from 1 to 100.
+ * on multiples 3 print Fizz, on multiples 5 print Buzz,
+ * on both print FizzBuzz
+ * Return: void
+ */
+int main(void)
+{
+int incr;
+
+for (incr = 1; incr <= 100; incr++)
+{
+if (incr % 3 == 0 && incr % 5 == 0)
+{
+printf("FizzBuzz ");
+}
+else if (incr % 3 == 0)
+{
+printf("Fizz ");
+}
+else if (incr % 5 == 0)
+{
+printf("Buzz ");
+}
+else
+{
+printf("%d ", incr);
+}
+}
+printf("\n");
+return (0);
+}
