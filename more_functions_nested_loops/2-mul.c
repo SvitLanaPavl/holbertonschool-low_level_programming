740fa874cd8124d78dd47d4eaa8e0293d6f1dd08
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-05-31 09:17:50 -0700

    Multiplication

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - Multiplies two integers.
+ * @a: first integer argument
+ * @b: second integer argument
+ * Description: prints the result of multiplication to the standard output.
+ * Return: 0 Success
+ */
+
+int mul(int a, int b)
+{
+	int result = a * b;
+
+	return (result);
+}
