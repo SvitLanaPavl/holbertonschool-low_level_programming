b17a6625c76f5575ce76087b4d987caef1eefe57
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-05-31 10:18:21 -0700

    Print more numbers

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -15,7 +15,7 @@ void more_numbers(void)
 
 for (count = 1; count <= 10; count++)
 {
-	for (number = 1; number <= 14; number++)
+	for (number = 0; number <= 14; number++)
 	{
 		if (number >= 10)
 		{
