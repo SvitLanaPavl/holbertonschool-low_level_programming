d4285459bdc017f39787dd1908dc331076f6e89d
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-05-31 12:06:24 -0700

    Diagonal line

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -16,9 +16,9 @@ void print_diagonal(int n)
 
 		for (count = 1; count <= n; count++)
 		{
-			if (count > 1)
+			if (count > 0)
 			{
-				for (space = 0; space <= count; space++)
+				for (space = 1; space <= count; space++)
 				{
 					_putchar(32);
 				}
