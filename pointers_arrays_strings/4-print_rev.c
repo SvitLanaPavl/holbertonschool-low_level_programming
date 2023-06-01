4486ec062239a077c401535a0a3fd773398e5284
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-06-01 09:52:30 -0700

    String reverse

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - Prints a string in reverse
+ * Description: prints a string in reverse
+ * @s: the string argument
+ * Return: void
+ */
+void print_rev(char *s);
+{
+	int length = strlen(s);
+	int count;
+
+	for (count = length - 1; count <= 0; count--)
+	{
+	_putchar(s[count]);
+	}
+	_putchar('\n');
+}
