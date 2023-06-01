383e0276e4c6b05c135ca46589d3133ba2c9e094
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-06-01 09:23:21 -0700

    String

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - Prints a string
+ * Description: prints a string folled by a new line to stdout
+ * @str: the string argument
+ * Return: str
+ */
+
+void _puts(char *str)
+{
+while (*str != '\0')
+{
+	_putchar(*str);
+	str++;
+}
+_putchar('\n');
+}
