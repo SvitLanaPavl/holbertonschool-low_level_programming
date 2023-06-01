8805ce02f791ced40c69d5809149c411b1c0e012
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-06-01 11:38:43 -0700

    Every other

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * puts - Prints every other character
+ * Description: Prints every other character of a string
+ * @str: the string argument
+ * Return: void
+ */
+void puts2(char *str)
+{
+int length = 0;
+
+while (str[length] != '\0')
+{
+if (length == 0)
+{
+_putchar(str[length]);
+}
+else if (length % 2 == 0)
+{
+_putchar(str[length]);
+}
+length++;
+}
+_putchar('\n');
+}
