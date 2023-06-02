2490fb0293a6848e33c1e71bbd92d6ab4ccf785c
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-06-02 13:48:37 -0700

    Print number

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -13,12 +13,12 @@ void print_number(int n)
 	{
 	_putchar('-');
 	n = -n;
-	_putchar(n = '0');
+	_putchar(n + '0');
 	}
 
 	while (n > 0)
 	{
-	_putchar(n % 10 + '0');
 	n /= 10;
+	_putchar(n % 10 + '0');
 	}
 }
