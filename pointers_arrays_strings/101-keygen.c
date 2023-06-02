6d6ed89ba8c714d9bb6a3dde0667817da9cf98dc
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-06-02 09:14:21 -0700

    Password

---
+++ pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+int password(int password_length)
+{
+int i;
+
+char list[] = "1234567890qwertyuiopasdfghjklzxcvbnm!@#$%^&*()_+-=QWERTYUIOPASDFGHJKLZXCVBNM[]{};':\"<>,.?/\|";
+
+srand(time(NULL));
+
+for (i = 0; i < password_length; i++)
+{
+	print("%c", list[rand() % (sizeof list - 1)]);
+}
+}
+
+main()
+{
+	int password_length;
+
+	printf("Enter length of the password\n");
+	scanf("%d", &password_length);
+	password(password_length);
+	return (0);
+}
