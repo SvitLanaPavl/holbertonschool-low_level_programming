743c583841f02cda41374d61df996911490adbea
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-06-07 14:28:48 -0700

    cap

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -11,17 +11,10 @@ char *cap_string(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (i == 0)
-		{
-			if ((str[i] >= 'a' && str[i] <= 'z'))
-			{
-				str[i] = str[i] - 32;
-				continue;
-			}
-		}
-		if (str[i] - 1 == ' ' || str[i] - 1 == '.' || str[i] - 1 == '\n' || str[i] - 1 == '\t' ||
-		str[i] - 1 == ',' || str [i] - 1 == '!' || str [i] - 1 == '?' || str[i] - 1 == '"' ||
-		str [i] - 1 == '(' || str[i] - 1 == ')' || str[i] - 1 == '}' || str[i] - 1 == '{')
+		if (i == 0 || str[i] - 1 == ' ' || str[i] - 1 == '.' || str[i] - 1 == '\n' ||
+		str[i] - 1 == '\t' || str[i] - 1 == ',' || str [i] - 1 == '!' ||
+		str [i] - 1 == '?' || str[i] - 1 == '"' || str [i] - 1 == '(' ||
+		str[i] - 1 == ')' || str[i] - 1 == '}' || str[i] - 1 == '{')
 		{
 			if (str[i] >= 'a' && str[i] <= 'z')
 			{
