c47ea8e00fca6f1a769c75e57e27306ced566bde
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-06-07 14:06:34 -0700

    Capitalize

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -19,20 +19,15 @@ char *cap_string(char *str)
 				continue;
 			}
 		}
-		if (str[i] == ' ' | str[i] == '.' | str[i] == '\n' | str[i] == '\t')
+		if (str[i] == ' ' | str[i] == '.' | str[i] == '\n' | str[i] == '\t' |
+		str[i] == ',' | str [i] == '!' | str [i] == '?' | str[i] == '"' |
+		str [i] == '(' | str[i] == ')' | str[i] == '}' | str[i] == '{')
 		{
 			i++;
 			if (str[i] >= 'a' && str[i] <= 'z')
 			{
 				str[i] = str[i] - 32;
 				continue;
-			}
-		}
-			else
-		{
-			if (str[i] >= 'A' && str[i] <= 'Z')
-			{
-				str[i] = str[i] + 32;
 			}
 		}
 	}
