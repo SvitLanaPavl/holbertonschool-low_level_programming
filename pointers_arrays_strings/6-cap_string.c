f92b4259fa61f0fcb76b683e047d3201efcd92d3
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-06-07 14:45:55 -0700

    Capitalize

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -13,7 +13,7 @@ char *cap_string(char *str)
 	{
 		while (!(str[i] >= 'a' && str[i] <= 'z'))
 		i++;
-		if (i == 0 || str [i - 1] == ',' || str[i - 1] == ' ' || str[i - 1] == '.' ||
+		if (i == 0 || str[i - 1] == ',' || str[i - 1] == ' ' || str[i - 1] == '.' ||
 		str[i - 1] == '\n' || str[i - 1] == '\t' || str[i - 1] == ',' ||
 		str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' ||
 		str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '}' ||
