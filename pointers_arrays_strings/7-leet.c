9e3ced17a543304bde997ed7eba818a40e059759
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-06-08 07:36:04 -0700

    Leet

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -12,7 +12,7 @@ char *leet(char *str)
 	char leet_table[] = "4433007711";
 	int i, x;
 
-for (i = 0; array[i] != '\0'; i++)
+for (i = 0; str[i] != '\0'; i++)
 {
 	for (x = 0; leet_table[x] < 10; x++)
 	{
