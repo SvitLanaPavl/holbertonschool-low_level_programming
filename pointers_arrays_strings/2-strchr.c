c5363693193dc5a69fa42a6531183a8c7cc9853d
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-06-08 09:42:15 -0700

    Strchr

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strchr - Locates a character in a string
+ * Description: Locates a character in a string
+ * @s: string argument
+ * @c: character argument
+ * Return: pointer to the first occurence of character c in a string s
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s++)
+	{
+	if (*s == c)
+		return (c);
+	}
+return (0);
+}
