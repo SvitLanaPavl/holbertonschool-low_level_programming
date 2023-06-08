1728c439ca44e411291073528662d26a65849c9c
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-06-08 12:07:58 -0700

    Strstr

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * _strstr - Locates a substring
+ * Description: locates the first occurence of the substring
+ * needle in the string haystack
+ * @haystack: string to be searched
+ * @needle: occurence to be located
+ * Return: a pointer to the begining of the located substring
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	char *s1, *s2;
+
+	while (*haystack != NULL)
+	{
+		if (*haystack == *needle)
+		{
+			s1 = haystack;
+			s2 = needle;
+		while (*s1 && *s2)
+		{
+			if (*s1 != *s2)
+			break;
+		s1++;
+		s2++;
+		}
+	if (*s2 == NULL)
+		return (haystack);
+		}
+	haystack++;
+	}
+return (NULL);
+}
