f4d622ba1a1205be2a78e76dbb994dc8627b92ba
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-06-09 07:35:38 -0700

    Rot13

---
+++ pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * rot13 - Encodes a string using rot 13
+ * Description: encodes a string using rot 13
+ * @str: string argument
+ * Return: encoded string
+ */
+char *rot13(char *str)
+{
+char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+int i, j;
+
+for (i = 0; str[i] != '\0')
+{
+for (j = 0; alphabet[j] != '\0', rot[j] != '\0'; j++)
+{
+	if (str[i] == alphabet[j])
+	{
+		str[i] = rot[j];
+	}
+}
+}
+return (str);
+}
