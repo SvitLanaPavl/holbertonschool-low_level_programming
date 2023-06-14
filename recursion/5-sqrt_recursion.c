b19a1aff428f580aacc710fe622aaf42c9a6dd37
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-06-14 10:09:42 -0700

    Sqrt recursively

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -10,16 +10,16 @@ int _sqrt_recursion(int n)
 	if (n == 0)
 	return (0);
 
-	int guess = x / 2;
-	return (square_root_averg(x, guess));
+	int guess = n / 2;
+	return (square_root_averg(n, guess));
 
-	int square_root_averg(int x, int guess)
+	int square_root_averg(int n, int guess)
 	{
-	int next_guess = (guess + x / guess) / 2;
+	int next_guess = (guess + n / guess) / 2;
 
 	if (fabs(guess - next_guess) < 1e-10)
 		return (guess);
 
-	return (square_root_averg(x, next_guess));
+	return (square_root_averg(n, next_guess));
 	}
 }
