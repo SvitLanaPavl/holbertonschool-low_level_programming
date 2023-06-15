996d26a2536b402ac7dc1bd837551e6ed9f86190
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-06-15 13:34:46 -0700

    Print the program name

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -3,15 +3,17 @@
 /**
 * main - entry point.
 * description: prints the name of the program
+* @argc: number of arguments passes to the program
+* @argv: array of pointers to the arguments
 * Return: Always 0
 */
 
-int main(int argc, char *argv[])
+int main((void)argc, char *argv[])
 {
 int i;
 char *name = argv[0];
 
-for (i = 0; i < argc; i++)
+for (i = 0; name[i] != '\0'; i++)
 _putchar(name[i]);
 
 _putchar('\n');
