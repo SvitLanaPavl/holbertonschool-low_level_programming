73ae3f2ea469e94db913c66925027404d230d8a6
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-06-15 13:50:40 -0700

    Print number

---
+++ argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* main - entry point.
+* description: prints the number of arguments
+* @argc: number of arguments passes to the program
+* @argv: array of pointers to the arguments
+* Return: Always 0
+*/
+int main(int argc, char *argv[])
+{
+int i;
+
+for (i = 0; i <= argv[0]; i++)
+_putchar(argc);
+
+_putchar('\n');
+return (0);
+}
