60949207aaa98ff62161c9c45fd7356fb910c82a
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-06-15 14:10:49 -0700

    Arguments

---
+++ argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* main - entry point.
+* description: prints all arguments
+* @argc: number of arguments passes to the program
+* @argv: array of pointers to the arguments
+* Return: Always 0
+*/
+
+int main(int argc, char *argv[])
+{
+int i;
+
+for (i = 0; i < argc; i++)
+{
+_putchar(argv[i]);
+_putchar('\n');
+}
+return (0);
+}
