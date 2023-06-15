b00c56294aafe6e9075a77d543501530f6c5c66f
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-06-15 14:55:04 -0700

    Print Sum

---
+++ argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+* main - adds positive numbers.
+* description: prints the result of adding positive numbers
+* of two arguments
+* @argc: number of arguments passes to the program
+* @argv: array of pointers to the arguments
+* Return: Always 0, if contains not digit prints "Error" return 1, if no
+* number passed print 0
+*/
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+	char chr;
+	bool isDigit = true;
+
+	if (argc == 0)
+	printf("0\n"), return (0);
+
+	for (i = 0; i < argc; i++)
+	{
+	char chr = argv[i];
+
+	if (c < '0' || c > '9')
+	isDigit = false, break;
+
+	if (isDigit)
+	sum += atoi(argv[i]);
+
+	else
+	printf("Error\n"), return (1);
+	}
+	printf("%d", sum);
+	return (0);
+}
