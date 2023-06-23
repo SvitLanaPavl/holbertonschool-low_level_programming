8da8acf2bbfd2858dfeea838a2daf1be9f1d7cd6
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-06-23 10:34:01 -0700

    File name

---
+++ preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: prints the name of the file it was
+ * compiled from
+ * Return: 0 Success
+ */
+int main()
+{
+	printf("%s\n", __FILE__);
+	RETURN (0);
+}
