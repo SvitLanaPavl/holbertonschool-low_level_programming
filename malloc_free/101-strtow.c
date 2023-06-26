6bdc93f88efd677e55eb4750533d91e5378a39b2
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-06-26 12:46:07 -0700

    strtow

---
+++ malloc_free/101-strtow.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * strtow - splits a string into words
+ * Description: splits a string into words
+ * @str: string argument
+ * Return: pointer to an array of strings
+ */
+char **strtow(char *str)
+{
+char **word = NULL;
+char *token = NULL;
+int i = 0;
+
+if (str == NULL || str[0] = '\0')
+	return (NULL);
+
+token = strtok(str, " ");
+while (token != NULL)
+{
+	word = realloc(word, sizeof(char *) * (i + 1));
+	word[i] = token;
+	i++;
+	token = strtok(NULL, " ");
+}
+
+word[i] = NULL;
+return (word);
+}
