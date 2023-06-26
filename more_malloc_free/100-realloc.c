00153071bd7b0091b993b64cf0b990086d798113
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-06-26 08:13:17 -0700

    Realloc

--- more_malloc_free/100-realloc.c
+++ more_malloc_free/100-realloc.c
@@ -4,7 +4,7 @@
  * Description: reallocates a memory block
  * @ptr: pointer to the memory previously allocated
  * @old_size: size in bytes of the allocated space for ptr
- * @new_size: new size in bytes of the new memory block 
+ * @new_size: new size in bytes of the new memory block
  * Return: void
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
@@ -13,12 +13,14 @@ void *new_ptr;
 
 if (new_size == 0)
 {
+if (ptr != NULL)
+{
 free(ptr);
+}
 return (NULL);
 }
 if (ptr == NULL)
 new_ptr = malloc(new_size);
-
 else
 {
 	if (new_size <= old_size)
@@ -26,7 +28,6 @@ else
 	return (ptr);
 	}
 new_ptr = malloc(new_size);
-
 if (new_ptr == NULL)
 {
 return (NULL);
