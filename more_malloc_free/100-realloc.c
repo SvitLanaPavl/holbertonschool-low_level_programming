1e2e39b7982270a9d89e670e5f1fc2f5d5966998
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-06-26 08:40:31 -0700

    Realloc

--- more_malloc_free/100-realloc.c
+++ more_malloc_free/100-realloc.c
@@ -11,21 +11,19 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 void *new_ptr;
 
-if (new_size == 0)
-{
-if (ptr != NULL)
+if (new_size == 0 && ptr != NULL)
 {
 free(ptr);
-}
 return (NULL);
 }
 if (ptr == NULL)
 {
 new_ptr = malloc(new_size);
+return (new_ptr);
 }
 else
 {
-if (new_size <= old_size)
+if (new_size == old_size)
 	{
 	return (ptr);
 	}
