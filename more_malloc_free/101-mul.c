76a1202fd45c418ff627c6ce20d14b4d596a7a4b
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-06-26 09:28:21 -0700

    Mul

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -8,7 +8,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int n1, n2, mul;
+	int n1, n2, mul, i, j;
 	char *num1, *num2;
 
 	if (argc != 3)
@@ -19,32 +19,17 @@ int main(int argc, char *argv[])
 	num1 = argv[1];
 	num2 = argv[2];
 
-	if (!is_digit(num1) || !is_digit(num2))
+	for (i = 0, j = 0; num1[i] != '\0' && num2[j] != '\0'; i++, j++)
+	{
+	if ((num1[i] < '0' && num1[i] > '9') || (num2[j] < '0' && num2[i] > '9'))
 	{
 	printf("Error\n");
 	exit(98);
+	}
 	}
 	n1 = atoi(num1);
 	n2 = atoi(num2);
 	mul = n1 * n2;
 	printf("%d\n", mul);
 	return (0);
-}
-/*is_digit - checks if the string is composed of digits
- *Description: checks if the string is composed of digits
- *str: string argument 
- *Return: 0 Success, 1 Failure
- */
-int is_digit(char *str)
-{
-int i;
-
-for (i = 0; str[i] != '\0'; i++)
-{
-if (str[i] < '0' || str[i] > '9')
-{
-return (0);
-}
-}
-return (1);
 }
