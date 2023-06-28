0eafe42c2261ff8e480a7792b8e2dbbeede51a1e
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-06-28 14:25:20 -0700

    New dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -9,6 +9,9 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+if (name == NULL || age < 0 || owner == NULL)
+return (NULL);
+
 dog_t *new_dog = malloc(sizeof(dog_t));
 
 if (new_dog == NULL)
@@ -17,12 +20,21 @@ free(new_dog);
 return (NULL);
 }
 
+new_dog->name = malloc(sizeof(strlen(name)+1));
+if (new_dog->name == NULL)
+{
+free(new_dog);
+return (NULL);
+}
+new_dog->owner = malloc(sizeof(strlen(owner)+1));
+if (new_dog->owner == NULL)
+{
+free(new_dog->name);
+return (NULL);
+}
 new_dog->name = strdup(name);
 new_dog->owner = strdup(owner);
 new_dog->age = age;
 
 return (new_dog);
-free(new_dog->name);
-free(new_dog->owner);
-free((void *)&new_dog->age);
 }
