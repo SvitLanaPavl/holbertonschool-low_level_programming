11eeedba96922bac163c0473cf568bb1feec31e1
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-06-28 13:14:58 -0700

    print dog

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+/**
+ * print_dog - print
+ * Description: initializes a variable of type struct dog
+ * @d: pointer to the dog structure
+ * Return: void
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	return;
+
+	printf("Name: %s\n", d->name ? d->name : "(nil)");
+	printf("Age: %2f\n", d->age == 0 ? "(nil)" : d->age);
+	printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
+}
