37a211680a52315f9230e17908fbd10fcf78639c
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-06-28 13:35:14 -0700

    New dog

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+/**
+ * new_dog - new struct
+ * Description: creates a copy of the old dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+if (new_dog == NULL)
+return NULL;
+
+new_dog->name = dog_t->name;
+new_dog->owner = dog_t->owner;
+new_dog->age = age;
+
+return (new_dog);
+}
