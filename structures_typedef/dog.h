97203d841847377551ed805d1d2b2558d2c07b41
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-06-28 13:02:14 -0700

    Dog init

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,10 @@
 #ifndef DOG_H
 #define DOG_H
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+#include <stdlib.h>
 /**
  * struct dog - Dog structure
  * @name: name member
