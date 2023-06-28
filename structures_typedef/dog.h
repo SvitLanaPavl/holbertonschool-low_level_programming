e86a99b02d6ee4a75097846f2f67a886e2cca08d
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-06-28 12:38:18 -0700

    Struct

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,12 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
-/* struct dog - Dog structure
+/*
+ * struct dog - Dog structure
  * @name: name member
  * @age: age member
  * @owner: owner member
  * Description: dog structure
  */
-struct dog {
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
