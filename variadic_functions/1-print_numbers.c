6b5b9c657433424dc9e608102c2d045568ac67c3
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-06-29 14:33:03 -0700

    Print  nums

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -10,15 +10,15 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i, number;
 	va_list ap;
-	va_start(ap, n);
 
+	va_start(ap, n);
 	if (!separator)
 	return;
 
 	for (i = 0; i < n; i++)
 	{
 	number = va_arg(ap, int);
-	printf("%d%s\n", number, separator);
+	printf("%d%s", number, separator);
 	}
 	va_end(ap);
 }
