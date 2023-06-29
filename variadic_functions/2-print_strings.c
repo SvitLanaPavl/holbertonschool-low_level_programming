6693eb67b73904c314e141597c48b79f6c135fdf
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-06-29 15:01:38 -0700

    Print str

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -15,7 +15,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
 	{
-	string = va_arg(ap, char);
+	string = va_arg(ap, int);
 	if (!separator)
 	separator = "";
 
