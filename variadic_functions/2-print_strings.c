548db2843bcd08dcf990e0ff0b52e4fbcd3700ed
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-06-29 16:56:18 -0700

    Print str

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -20,7 +20,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	separator = "";
 
 	if (i < n - 1 && separator != NULL)
-	printf("%s", separator);
+	printf("%s", string);
 
 	if (!string)
 	printf("nil");
