a0770749f0bdb5c469ab7a745f06fd64190cdadd
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-06-29 11:29:01 -0700

    Search index

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -12,16 +12,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 int i;
 
-if (array == NULL && cmp == NULL)
-return;
-
 if (size <= 0) return (-1);
 
 for (i = 0; i < size; i++)
 {
-if (array[i] >= 0 && array[i] <= 9)
+if (cmp(array[i]) != 0)
 {
-cmp(array[i]);
 return (i);
 }
 }
