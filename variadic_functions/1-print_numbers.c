4c6db7e8b4f630f39736d90acacec860296a3ebe
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-06-29 14:48:43 -0700

    Print nums

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -15,7 +15,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 	number = va_arg(ap, int);
-	if (i == n - 1)
+	if (i == n - 1 && !separator)
 	{
 	printf("%d", number);
 	}
