68abf25e2db78953dec141042ad4bb8e258b3bd8
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-06-29 13:40:01 -0700

    Calc

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -9,7 +9,7 @@
  */
 int (*get_op_func(char *s))(int, int)
 {
-	int i;
+	int i = 0;;
 
 	op_t ops[] = {
 	{"+", op_add},
@@ -22,10 +22,8 @@ int (*get_op_func(char *s))(int, int)
 while (ops[i].op != NULL)
 {
 if (strcmp(s, ops[i].op) == 0)
-{
 return (ops[i].f);
-}
 i++;
 }
-return (NULL);
+return (0);
 }
