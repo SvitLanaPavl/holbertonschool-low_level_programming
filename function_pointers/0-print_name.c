1513960cfc9443bf6709c3a43ea6b4d467977316
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-06-29 07:54:28 -0700

    Print name

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+/**
+ * print_name - Prints a name
+ * Desription: function prints name
+ * @name: pointer to a string - name to be printed
+ * @f: pointer to a function
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+int i;
+for (i = 0; i < strlen(name); i++)
+{
+if (i == (strlen(name) - 1))
+f(name[i];
+
+else
+_putchar(f(name[i]));
+_putchar(' ');
+}
+}
