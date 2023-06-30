6ffee6251df21d29552243caf0c4545974bbef41
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-06-30 15:02:05 -0700

    Print all

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,4 +1,43 @@
 #include "variadic_functions.h"
+/**
+ * print_char - prints character
+ * @ap: format arg
+ */
+void print_char(va_list ap)
+{
+	printf("%c", va_arg(ap, int));
+}
+/**
+ * print_int - prints integer
+ * @ap: format arg
+ */
+void print_int(va_list ap)
+{
+	printf("%d", va_arg(ap, int));
+}
+/**
+ * print_float - prints float
+ * @ap: format arg
+ */
+void print_float(va_list ap)
+{
+	printf("%f", va_arg(ap, double));
+}
+/**
+ * print_string - prints string
+ * @ap: format arg
+ */
+void print_string(va_list ap)
+{
+	char *s = va_arg(ap, char *);
+
+	if (!s)
+	{
+	printf("(nil)");
+	return;
+	}
+	printf("%s", s);
+}
 void print_all(const char * const format, ...) {
 print_f elems[] = {
 {"c", print_char},
