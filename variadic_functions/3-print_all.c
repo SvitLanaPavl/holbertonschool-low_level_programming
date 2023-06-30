cdc2d087b3f9b940327c7f70ac4a3dcc8b6d9f9c
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-06-30 16:26:35 -0700

    Changed structure

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,81 +1,45 @@
 #include "variadic_functions.h"
 /**
- * print_char - prints character
- * @ap: format arg
+ * print_all - prints all args
+ * @format: format arg
+ * Return: void
  */
-void print_char(va_list ap)
-{
-	printf("%c", va_arg(ap, int));
-}
-/**
- * print_int - prints integer
- * @ap: format arg
- */
-void print_int(va_list ap)
-{
-	printf("%d", va_arg(ap, int));
-}
-/**
- * print_float - prints float
- * @ap: format arg
- */
-void print_float(va_list ap)
-{
-	printf("%f", va_arg(ap, double));
-}
-/**
- * print_string - prints string
- * @ap: format arg
- */
-void print_string(va_list ap)
-{
-	char *s = va_arg(ap, char *);
-
-	if (!s)
-	{
-	printf("(nil)");
-	return;
-	}
-	printf("%s", s);
-}
 void print_all(const char * const format, ...) {
-print_f elems[] = {
-{"c", print_char},
-{"i", print_int},
-{"f", print_float},
-{"s", print_string},
-{NULL, NULL}
-  };
-unsigned int i = 0;
-char *separator = "";
+unsigned int flag, i = 0;
+char *string;
 va_list ap;
-va_start(ap, format);
 
-if (format[i]) {
-print_all(format + 1, ap);
+while (format)
+{
+	va_start(ap, format);
+while (format[i]) {
+flag = 1;
 switch (format[i]) {
 case 'c':
-printf("%s", separator);
-elems[0].f(ap);
-separator = ", ";
+printf("%c", va_arg(ap, int));
 break;
 case 'i':
-printf("%s", separator);
-elems[1].f(ap);
-separator = ", ";
+printf("%d", va_arg(ap, int));
 break;
 case 'f':
-printf("%s", separator);
-elems[2].f(ap);
-separator = ", ";
+printf("%f", va_arg(ap, double));
 break;
 case 's':
-printf("%s", separator);
-elems[3].f(ap);
-separator = ", ";
+string = va_arg(ap, char *);
+if (!string)
+	string = "(nil)";
+printf("%s", string);
+break;
+default:
+flag = 0;
 break;
     }
-  }
-  va_end(ap);
-  printf("\n");
+if (format[i + 1] && flag)
+	print(", ";
+i++;
+}
+va_end(ap);
+break;
+}
+printf("\n");
 }
