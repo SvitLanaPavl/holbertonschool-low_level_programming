8f664e690f89de21e991b7738fc5e086289c531d
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-06-29 18:02:41 -0700

    Print all

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,45 @@
+#include "variadic_functions.h"
+/**
+ * print_all - prints anything
+ * Description: prints anything
+ * @format: list of types of arguments passed to the function
+ * Return: void
+ */
+void print_all(const char * const format, ...)
+{
+int i;
+char c;
+float f;
+char *s;
+va_list ap;
+va_start(ap, format);
+
+while (*format != '\0')
+{
+	switch (*format)
+	case 'c':
+		c = va_arg(ap, int);
+		printf("%c", c);
+		break;
+	case 'f':
+		f = va_arg(ap, float);
+		printf("%f", f);
+		break;
+	case 'i':
+		c = va_arg(ap, int);
+		printf("d", i);
+		break;
+	case 's':
+		s = va_arg(ap, char *);
+		if (s == NULL)
+		printf("(nil)");
+
+		else
+		printf("%s", s);
+		break;
+	default:
+		break;
+format++;
+}
+va_end(ap);
+}
