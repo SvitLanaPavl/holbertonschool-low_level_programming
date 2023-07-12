4085663338e5e9a2613af6481cdb61857f12a550
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-07-12 11:49:36 -0700

    null

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -2,7 +2,7 @@
 /**
  * print_list - prints all the elements of the structure
  * Description: prints all the elements of the structure
- * @h: pointer to structure
+ * @h: pointer to head
  * Return: the number of nodes
  */
 size_t print_list(const list_t *h)
@@ -12,7 +12,7 @@ int count = 0;
 
 while (temp)
 {
-if (!temp->str || !temp->str[0])
+if (!temp->str)
 {
 printf("[0] (nil)\n");
 count++;
