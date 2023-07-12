64cd2e712ea12ffacd104d6d571c4e5e14f23e1e
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-07-12 12:40:12 -0700

    first push

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * add_node - adds a node at the beginning of a list
+ * Description: adds a node at the beginning of the list
+ * @head: pointer to head
+ * @str: string pointer
+ * Return: address of the new element or NULL it it failed
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+list_t *newNode = malloc(sizeof(list_t));
+if (!newNode)
+return (NULL)
+
+newNode->str = char *strdup(const char *str);
+if (!newNode->str)
+{
+	free(newNode);
+	return (NULL);
+}
+new_node->len = strlen(str);
+newNode->next = (*head);
+(*head) = newNode;
+return (&newNode);
+}
