1498d1b5465cf91bf93dc42bdbff12dcd9b4ec81
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-07-13 14:17:34 -0700

    one function

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -1,22 +1,5 @@
 #include "main.h"
 /**
- * count_bits - counts bits
- * Description: counts bits
- * @n: number
- * Return: count
- */
-int count_bits(int n)
-{
-int count = 0;
-
-while (n > 0)
-{
-	count += (n & 1);
-	n >>= 1;
-}
-return (count);
-}
-/**
  * flip_bits - flips bits
  * Description: flips bits
  * @n: one number
@@ -26,6 +9,12 @@ return (count);
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-unsigned long int u = n ^ m;
-return (count_bits(u));
+unsigned long int u = n ^ m, count = 0;
+
+while (n > 0)
+{
+        count += (n & 1);
+        n >>= 1;
+}
+return (count);
 }
