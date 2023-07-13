0f837e8b1e9fd3fc2e728c819a268d2a4c2cc577
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-07-13 11:26:13 -0700

    some changes

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -15,13 +15,13 @@ int i = 0;
 if (b == NULL)
 return (0);
 
+while (*b)
+{
 while (b[i] == '0' || b[i] == '1')
 {
 if (b[i] != '0' && b[i] != '1')
-break;
+return (0);
 
-else
-{
 value <<= 1;
 value += b[i] - '0';
 i++;
