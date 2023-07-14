4e437d6d967abc250f55b499c06bf3a54772d49e
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-07-14 14:51:11 -0700

    first commit

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * append_text_to_file - appends text at the end of the file
+ * Description: appends text at the end of the file
+ * @filename: name of the file to create
+ * @text_content: null terminated string to write to the file
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+int file_descr;
+size_t bytes;
+
+if (!filename)
+return (-1);
+
+if (!text_content)
+{
+file_descr = open(filename, O_RDONLY);
+if (file_descr < 1)
+return (-1);
+
+close(file_descr);
+return (1);
+}
+
+file_descr = open(filename, O_APPEND);
+if (file_descr < 0)
+return (-1);
+
+bytes = write(file_descr, text_content, strlen(text_content));
+if (bytes < 0)
+{
+close (file_descr);
+return (-1);
+}
+
+close(file_descr);
+return (1);
+}
